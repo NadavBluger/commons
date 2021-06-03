84f5bc7ffa8e1db585e1cd542b4a5e562f5ea3bd
Nadav <nbluger@gmail.com>
2021-06-03 17:26:06 +0300

    RabbitMQ Agent Completion

--- commons/RabbitMQ.py
+++ commons/RabbitMQ.py
@@ -1,9 +1,10 @@
 """
-A a-synchros RabbitMQ agent meant to handle both reading and writing to the rabbitMQ
+A a-synchronise RabbitMQ agent meant to handle both reading and writing to the rabbitMQ
 
 Example for a configuration in the JSON format
 {
-    connection_url:'amqp://amdin:admin@127.0.0.1/'
+    credentials:'username:password'
+    host: '0.0.0.0'
     output_exchanges:
         [
             ('Reader_out',{type:fanout, durable: True #can contain any argument that the exchange declaration will take}),
@@ -16,9 +17,6 @@ Example for a configuration in the JSON format
 """
 import aio_pika
 import configurations
-if __name__ == '__main__':
-    import asyncio
-    from queue import Queue
 
 
 class OutputExchange:
@@ -35,64 +33,65 @@ class InputQueue:
 
 class RabbitMQAgent:
     def __init__(self, config: configurations.Configuration, loop):
-        self.connection_url = config.connection_url
+        self.username, self.password = config.credentials.split(":")
+        self.host = config.host
         self.output_exchanges = [OutputExchange(name, details) for name, details in config.output_exchanges]
         self.input_exchanges_names = config.input_exchanges
         self.input_queue = InputQueue(*config.input_queue)
         self.loop = loop
 
     @staticmethod
-    def manual_init(connection_url, output_exchanges: [tuple], input_exchanges: [tuple], input_queue, loop):
-        """Build a rabbitMQ agent from arguments given manualy rather than packeged as configuration object"""
+    def manual_init(credentials, host, output_exchanges: [tuple], input_exchanges: [tuple], input_queue, loop):
+        """Build a rabbitMQ agent from arguments given manually rather than packaged as configuration object"""
         config = configurations.Configuration(["connection_url", "output_exchanges", "input_exchanges", "input_queue"])
-        config.connection_url = connection_url
+        config.credentials = credentials
+        config.host = host
         config.output_exchanges = output_exchanges
         config.input_exchanges = input_exchanges
         config.input_queue = input_queue
         return RabbitMQAgent(config, loop)
 
     async def connect(self):
-        return await aio_pika.connect_robust(self.connection_url, loop=self.loop)
+        return await aio_pika.connect_robust(login=self.username,
+                                             password=self.password,
+                                             loop=self.loop,
+                                             host=self.host)
 
-    async def write(self, message, routing_key = "/"):
+    async def write(self, message, routing_key="/", connection=None):
         """
         Write a single message to all the agents output exchanges
         :param message: The contents of the message to be written
         :param routing_key: The routing key to route to the message
+        :param connection: a connection to the queue manager
         :return:
         """
-        async with await self.connect() as con:
+        if not connection:
+            con = await self.connect()
+        else:
+            con = connection
+        async with con:
             channel = await con.channel()
             for exchange in self.output_exchanges:
                 try:
                     exchange = await channel.declare_exchange(exchange.name, **exchange.details)
                 finally:
-                    await exchange.publish(message, routing_key)
+                    await exchange.publish(aio_pika.Message(message.encode()), routing_key=routing_key)
 
-    async def read(self):
+    async def read(self, connection=None):
         """
         Read a single message from the agents queue and return it
+        :param connection: a connection to the queue manager
         :return:
         """
-        async with await self.connect() as con:
+        if not connection:
+            con = await self.connect()
+        else:
+            con = connection
+        async with con:
             channel = await con.channel()
             try:
-                queue = channel.declare_queue(self.input_queue.name, **self.input_queue.details)
+                queue = await channel.declare_queue(self.input_queue.name, **self.input_queue.details)
             finally:
                 for exchange in self.input_exchanges_names:
-                    queue.bind(exchange)
-            return queue.consume()
-
-
-if __name__ == '__main__':
-    configuration = {
-        "connection_url": "",
-        "output_exchanges": [],
-        "input_exchanges": [],
-        "input_queue": ()
-
-    }
-    confi = configurations.JSONConfiguration([])
-    confi.parse_json(configuration)
-    loop = asyncio.get_event_loop()
-    agent = RabbitMQAgent(confi, loop)
+                    await queue.bind(exchange)
+            return await queue.get(no_ack=True)
